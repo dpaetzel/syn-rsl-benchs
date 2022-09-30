70af7031c2d2560503876d9f1cb2f4893bf02da3
David Pätzel <david.paetzel@posteo.de>
2022-09-30 15:37:58 +0200

    Add configurable radius of crowded region

--- gen_data.py
+++ gen_data.py
@@ -5,38 +5,47 @@ import scipy.stats as st  # type: ignore
 
 
 @click.command()
-@click.option("-K", "--n-rules", default=3, type=int)
-@click.option("-d", "--dimensions", default=1, type=int)
-@click.option("-s", "--seed", default=1, type=int)
+@click.option("-K",
+              "--n-components",
+              default=3,
+              type=int,
+              help="Number of components to use")
+@click.option("-d",
+              "--dimensions",
+              default=1,
+              type=int,
+              help="Number of input dimensions to generate data for")
+@click.option("-s",
+              "--seed",
+              default=1,
+              type=int,
+              help="Random seed to be used")
+@click.option("--crowd-reg-radius",
+              default=0.25,
+              type=float,
+              help="99% radius of the crowded region")
 @click.argument("N", type=int)
-def cli(n_rules, dimensions, seed, n):
+def cli(n_components, dimensions, seed, n, crowd_reg_radius):
 
     np.random.seed(seed)
 
     # TODO Generate or configure the number of crowded regions first.
-    # n_crowded_regions = ...
-    # centers_crowded_regions = ...
-    # centers = ...
-    # spreads = ...
-
-    # TODO Consider clipping to 0.99 or so (however, this has to depend on
-    # dimensionality since in higher dimensions most of the space is further out
-    # than 0.99)
-
-    # TODO Several things should depend on dimensionality for [-1, 1] to make
-    # sense as a domain
 
     # Scale was determined visually.
     center_crowded_region = st.norm(
         loc=0.0, scale=0.2).rvs(dimensions).reshape(dimensions)
 
+    cov_crowded_region = np.eye(dimensions) * (
+        crowd_reg_radius**2 / st.chi2(df=dimensions).ppf(q=0.99))
+
     # Generate one less rule because we add a default rule later.
-    centers = st.multivariate_normal(mean=center_crowded_region,
-                                     cov=1).rvs(n_rules - 1).reshape(
-                                         n_rules - 1, dimensions)
+    centers = st.multivariate_normal(
+        mean=center_crowded_region,
+        cov=cov_crowded_region).rvs(n_components - 1).reshape(
+            n_components - 1, dimensions)
 
     # TODO How much overlap do we want?
-    spreads = st.uniform().rvs((n_rules - 1, dimensions))
+    spreads = st.uniform().rvs((n_components - 1, dimensions))
 
     # Add a default rule so we don't have to check whether there is a rule
     # matching.
@@ -45,32 +54,32 @@ def cli(n_rules, dimensions, seed, n):
 
     def match(x):
         """
-        Values of the `n_rules` matching functions for the given input.
+        Values of the `n_components` matching functions for the given input.
         """
 
         # One condition per rule (rows) per dimension (columns).
-        conds = (centers - spreads < x) & (x < centers + spreads)
+        conds = (centers - spreads <= x) & (x <= centers + spreads)
 
         # A rule only matches if all its conditions are fulfilled.
         return np.all(conds, axis=1).astype(float)
 
     # d coefficients per rule.
-    coeffs = st.uniform(loc=-4, scale=8).rvs((n_rules, dimensions))
+    coeffs = st.uniform(loc=-4, scale=8).rvs((n_components, dimensions))
 
     # One intercept per rule.
-    intercepts = st.uniform(loc=-4, scale=8).rvs(n_rules)
+    intercepts = st.uniform(loc=-4, scale=8).rvs(n_components)
 
     def output_local(x):
         """
-        Values of the `n_rules` local models for the given input.
+        Values of the `n_components` local models for the given input.
         """
         return np.sum(x * coeffs, axis=1) + intercepts
 
     # Noise is fixed per rule (also, assume same noise for each dimension).
-    std_noises = st.gamma(a=1.0, scale=1.0).rvs(n_rules)
+    std_noises = st.gamma(a=1.0, scale=1.0).rvs(n_components)
 
     # One mixing coefficient per rule.
-    mixing_weights = st.uniform().rvs(n_rules)
+    mixing_weights = st.uniform().rvs(n_components)
 
     def output(x):
         """
